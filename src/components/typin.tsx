95677cd985e519c1d3b4c2a126e439440c190060
Ally-keyz <manzialpe@gmail.com>
2025-10-16 06:47:52 -0600

    Finished login page design

--- src/components/typin.tsx
+++ src/components/typin.tsx
@@ -4,7 +4,7 @@ import Lottie from 'lottie-react'
 type propsTypes = {
   data : {} ,
   width : number ,
-  height : number
+  height : number,
 }
 
 function Typin({data , width , height} : propsTypes) {
---
+++ types.d.ts
@@ -0,0 +1,15 @@
+
+interface UserLoginInfo {
+    email : string ,
+    password : string
+}
+interface User extends UserLoginInfo {
+    name : string
+}
+
+
+export default User ;
+
+
+
+
